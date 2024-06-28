8d3ef7eff89e8c7a3dbac88eee46cae28bd6e9e3
Andrew Zhao <andrew2002zhao@gmail.com>
2024-06-28 17:05:41 +0000

    created a launch for map saving and updated cartographer launch

--- cartographer_slam/launch/cartographer.launch.py
+++ cartographer_slam/launch/cartographer.launch.py
@@ -1,5 +1,5 @@
 import launch 
-from launch.description import LaunchDescription
+from launch import LaunchDescription
 from launch_ros.actions import Node
 import os
 from ament_index_python.packages import get_package_share_directory
