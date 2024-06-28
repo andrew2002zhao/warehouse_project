ec9579b42242bbdb7789ef6b1169d5396f40b3dd
Andrew Zhao <andrew2002zhao@gmail.com>
2024-06-28 17:47:33 +0000

    fixed errors for mapping server

--- map_server/launch/map_server.launch.py
+++ map_server/launch/map_server.launch.py
@@ -8,34 +8,35 @@ def generate_launch_description():
 
     map_server_config = os.path.join(get_package_share_directory('map_server'), 'config')
     map_file = os.path.join(map_server_config, 'warehouse_map_sim.yaml')
-    rviz_file = os.path.join(get_package_share_directory('map_server'), 'rviz',' rviz_config.rviz')
+    rviz_file = os.path.join(get_package_share_directory('map_server'), 'rviz', 'rviz_config.rviz')
+
     container = LaunchDescription(
-    [
-        Node(
-        package='nav2_map_server',
-        executable='map_server',
-        name='map_server',
-        output='screen',
-        parameters=[{'use_sim_time': True}, 
-                        {'yaml_filename':map_file}]
-        ),
-        Node(
-            package='nav2_lifecycle_manager',
-            exectuable='lifecycle_manage',
-            name='lifecycle_manager_mapper',
+        [
+            Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
             output='screen',
-            parameters=[{'use_sim_time', True},
-                        {'autostart': True},
-                        {'node_names':['map_server']}]
-        ),
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen',
-            arguments=['-d', rviz_file]
-        )
-    ]
+            parameters=[{'use_sim_time': True}, 
+                            {'yaml_filename': map_file}]
+            ),
+            Node(
+                package='nav2_lifecycle_manager',
+                executable='lifecycle_manager',
+                name='lifecycle_manager_mapper',
+                output='screen',
+                parameters=[{'use_sim_time': True},
+                            {'autostart': True},
+                            {'node_names':['map_server']}]
+            ),
+            Node(
+                package='rviz2',
+                executable='rviz2',
+                name='rviz2',
+                output='screen',
+                arguments=['-d', rviz_file]
+            )
+        ]
     
     )
     return container
